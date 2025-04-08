7e68e64aab3ec1b6b7d3072e0c8c0be661b8821e
gauravrankmantra <gauravlakherarankmantra@gmail.com>
2025-04-08 15:30:53 +0530

    fixed

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -70,14 +70,14 @@ module.exports.logIn = asyncHandler(async (req, res, next) => {
 
   res.cookie('accessToken', accessToken, {
     httpOnly: true,
-    secure: isProduction,
+    secure: true,
     sameSite: "none",
     maxAge: 24 * 60 * 60 * 1000
   });
 
   res.cookie('refreshToken', refreshToken, {
     httpOnly: true,
-    secure: isProduction,
+    secure: true,
     sameSite:  'none',
     maxAge: 7 * 24 * 60 * 60 * 1000
   });
--- src/routes/api/v1/auth.routes.js
+++ src/routes/api/v1/auth.routes.js
@@ -40,14 +40,14 @@ router.get(
 
     res.cookie('accessToken', accessToken, {
       httpOnly: true,
-      secure: isProduction,
+      secure: true,
       sameSite: 'none',
       maxAge: 24 * 60 * 60 * 1000
     });
 
     res.cookie('refreshToken', refreshToken, {
       httpOnly: true,
-      secure: isProduction,
+      secure: true,
       sameSite: 'none',
       maxAge: 7 * 24 * 60 * 60 * 1000
     });
